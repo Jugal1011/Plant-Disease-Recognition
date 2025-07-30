31de7a01a1c1d5cebb63b8cc5deba34354bc8232
JugalSoni <sonijugal2002@gmail.com>
2025-07-30 22:58:51 +0530

    Added FILE_ID in .env and updated gdown

--- download_model.py
+++ download_model.py
@@ -1,9 +1,17 @@
 import gdown
 import os
+from dotenv import load_dotenv
 
-# Google Drive shareable link (replace with your actual link)
-url = 'https://drive.google.com/uc?id=1ueTaKohf3BccqmopOrEYFCT1SqrAundH'
+# Load .env file (local only)
+load_dotenv()
 
+file_id = os.environ.get('FILE_ID')
+if not file_id:
+    raise ValueError("FILE_ID not set in environment variables.")
+
+# Use fuzzy URL to support confirmation bypass
+url = f'https://drive.google.com/uc?id={file_id}'
 output = os.path.join('model', 'plant-disease-recognition.hdf5')
+
 os.makedirs('model', exist_ok=True)
-gdown.download(url, output, quiet=False)
+gdown.download(url, output, quiet=False, fuzzy=True)
